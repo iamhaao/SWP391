d8c21876a43e429d2cf35bd9aada2ff38d788e24
Le Hoang Nhat Linh <lehoangnhatlinh0806@gmail.com>
2023-05-25 21:32:50 +0700

    login logout

--- src/java/Controller/LoginServlet.java
+++ src/java/Controller/LoginServlet.java
@@ -12,12 +12,12 @@ import Models.Order;
 import Models.Product;
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;    
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;    
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
